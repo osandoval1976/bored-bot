6eb4fc22aa3b68499a91f1016d3d5b3264b96c77
Racso <oscarhs0576@gmail.com>
2025-12-18 16:19:29 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("get-activity").addEventListener("click", function() {
-  fetch("https://apis.scrimba.com/bored/api/activity")
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById("activity").textContent = data.activity
-      document.getElementById("title").textContent = "🦾 HappyBot🦿"
-      document.querySelector("main").classList.add("fun")
-    })
-})
