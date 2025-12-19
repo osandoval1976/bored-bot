70226bed7f83b240f6b4a192f43d90eaf5a005c8
Racso <oscarhs0576@gmail.com>
2025-12-18 20:19:57 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
+function getActivityIdea() {
+    fetch("https://apis.scrimba.com/bored/api/activity")
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById("idea").textContent = data.activity
+            document.body.classList.add("fun")
+            document.getElementById("title").textContent = "🦾 HappyBot🦿"
+        })
+}
 
-
-
-document.getElementById("get-activity").addEventListener("click", function() {
-  fetch("https://apis.scrimba.com/bored/api/activity")
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById("activity").textContent = data.activity
-      document.getElementById("title").textContent = "🦾 HappyBot🦿"
-      document.querySelector("main").classList.add("fun")
-    })
-})
+document.getElementById("bored-button").addEventListener("click", getActivityIdea)
